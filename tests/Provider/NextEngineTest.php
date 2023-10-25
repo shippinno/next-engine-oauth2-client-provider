6ff4055cfe3866bbe49e3176dc4e5debb7ee8f22
kimurasayo <kimura34@hotmail.com>
2023-10-25 15:31:57 +0900

    PHP8.2対応

--- tests/Provider/NextEngineTest.php
+++ tests/Provider/NextEngineTest.php
@@ -4,10 +4,14 @@ namespace Shippinno\NextEngine\OAuth2\Client\Tests\Provider;
 
 use GuzzleHttp\ClientInterface;
 use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 use Shippinno\NextEngine\OAuth2\Client\Provider\NextEngineProvider;
+use GuzzleHttp\Psr7\Stream;
+use InvalidArgumentException;
 
 class NextEngineTest extends TestCase
 {
@@ -16,7 +20,7 @@ class NextEngineTest extends TestCase
      */
     protected $provider;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->provider = new NextEngineProvider([
             'clientId' => 'mock_client_id',
@@ -45,14 +49,20 @@ class NextEngineTest extends TestCase
 
     public function testGetAccessToken()
     {
+        $stream = $this->createMock(StreamInterface::class);
+        $stream
+            ->method('__toString')
+            ->willReturn('{"access_token":"mock_access_token","scope":"email","token_type":"bearer"}');
+
         $response = Mockery::mock(ResponseInterface::class);
-        $response->shouldReceive('getBody')->andReturn('{"access_token":"mock_access_token", "token_type":"bearer"}');
+        $response->shouldReceive('getBody')->andReturn($stream);
         $response->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
-        $response->shouldReceive('getStatusCode')->andReturn(200);
-        $client = Mockery::mock(ClientInterface::class);
+
+        $client = Mockery::mock('GuzzleHttp\ClientInterface');
         $client->shouldReceive('send')->once()->andReturn($response);
         $this->provider->setHttpClient($client);
-        $token = $this->provider->getAccessToken('authorization_code', ['uid' => uniqid(), 'state' => uniqid()]);
+
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
         $this->assertEquals('mock_access_token', $token->getToken());
         $this->assertNull($token->getExpires());
         $this->assertNull($token->getRefreshToken());
@@ -60,19 +70,27 @@ class NextEngineTest extends TestCase
     }
 
     /**
-     * @expectedException League\OAuth2\Client\Provider\Exception\IdentityProviderException
+     * @expectException \League\OAuth2\Client\Provider\Exception\IdentityProviderException
+     * @expectedExceptionMessage ERROR_DESCRIPTION
+     * @expectExceptionCode 400
      */
     public function testExceptionThrownWhenErrorObjectReceived()
     {
-        $message = uniqid();
         $status = rand(400, 600);
+        $stream = $this->createMock(StreamInterface::class);
+        $stream
+            ->method('__toString')
+            ->willReturn('{"error_description":"ERROR_DESCRIPTION","error":"some_error"}');
+
         $postResponse = Mockery::mock(ResponseInterface::class);
-        $postResponse->shouldReceive('getBody')->andReturn('{"result":"error","message":"' . $message . '"}');
-        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $postResponse->shouldReceive('getBody')->andReturn($stream);
+        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'application/json;charset=UTF-8']);
         $postResponse->shouldReceive('getStatusCode')->andReturn($status);
         $client = Mockery::mock('GuzzleHttp\ClientInterface');
         $client->shouldReceive('send')->once()->andReturn($postResponse);
         $this->provider->setHttpClient($client);
+
+        $this->expectException(InvalidArgumentException::class);
         $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
     }
 
